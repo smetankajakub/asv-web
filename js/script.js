335f0b32770455eedc4bb90ea8089cf7d1e7a390
xsmetankaj <smetanka321@gmail.com>
2020-10-17 18:42:03 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,43 +1,48 @@
 var x = 0;
 $(document).ready(function(){
-    $('#pr2').fadeOut();
+    let heightFirst = $('#first').height() - 10;
+    let heightSecond = $('#second').height() + heightFirst;
+    let heightThird = $('#third').height() + heightSecond;
+    let heightFourth = $('#fourth').height() + heightThird;
+    let heightFifth = $('#fifth').height() + heightFourth;
+    $('#pr2').fadeOut(1);
     $('#pr3').fadeOut(1);
     $('#pr4').fadeOut(1);
     $('#pr5').fadeOut(1);
     $("#list").scroll(function(){
     //   $("span").text( x+= 1);
       var y = $('div #list').scrollTop();
-    if (y < 115) {
-        $('#pr1').fadeIn(800);
-        $('#pr2').fadeOut(1);
-        $('#pr3').fadeOut(1);
-        $('#pr4').fadeOut(1);
-        $('#pr5').fadeOut(1);
-    } else if (y > 115 && y < 2*115) {
-        $('#pr2').fadeIn(800);
-        $('#pr1').fadeOut(1);
-        $('#pr3').fadeOut(1);
-        $('#pr4').fadeOut(1);
-        $('#pr5').fadeOut(1);   
-    } else if( y > 2*115 && y < 3*115){
-        $('#pr3').fadeIn(800);
-        $('#pr2').fadeOut(1);
-        $('#pr4').fadeOut(1);
-        $('#pr5').fadeOut(1);  
-        $('#pr1').fadeOut(1);
-    } else if( y > 3*115 && y < 4*115){
-        $('#pr4').fadeIn(800);
-        $('#pr1').fadeOut(1);
-        $('#pr2').fadeOut(1);
-        $('#pr3').fadeOut(1);  
-        $('#pr5').fadeOut(1);
-    } else if( y > 4*110 && y < 5*115){
-        $('#pr5').fadeIn(800);
-        $('#pr1').fadeOut(1);
-        $('#pr2').fadeOut(1);
-        $('#pr3').fadeOut(1);  
-        $('#pr4').fadeOut(1);
-    }
+        if (y < heightFirst) {
+            $('#pr1').fadeIn(800);
+            $('#pr2').fadeOut(1);
+            $('#pr3').fadeOut(1);
+            $('#pr4').fadeOut(1);
+            $('#pr5').fadeOut(1);
+        } else if (y >= heightFirst && y < heightSecond) {
+            $('#pr2').fadeIn(800);
+            $('#pr1').fadeOut(1);
+            $('#pr3').fadeOut(1);
+            $('#pr4').fadeOut(1);
+            $('#pr5').fadeOut(1);   
+        } else if( y >= heightSecond && y < heightThird){
+            $('#pr3').fadeIn(800);
+            $('#pr2').fadeOut(1);
+            $('#pr4').fadeOut(1);
+            $('#pr5').fadeOut(1);  
+            $('#pr1').fadeOut(1);
+        } else if( y >= heightThird && y < heightFourth){
+            $('#pr4').fadeIn(800);
+            $('#pr1').fadeOut(1);
+            $('#pr2').fadeOut(1);
+            $('#pr3').fadeOut(1);  
+            $('#pr5').fadeOut(1);
+        } else if( y >= heightFourth){
+            $('#pr5').fadeIn(800);
+            $('#pr1').fadeOut(1);
+            $('#pr2').fadeOut(1);
+            $('#pr3').fadeOut(1);  
+            $('#pr4').fadeOut(1);
+        }
     console.log('y' + y);
       console.log('x');
     });
